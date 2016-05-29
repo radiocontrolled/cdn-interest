86e0d8850e0099d369056970573c7be68925366b
radiocontrolled <radiocontrolled@gmail.com>
2016-05-29 22:13:48 +0100

    full chart

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@
   "use strict";
 
   var height, width, svg, widthInner; 
-
+  
 
   // RESPONSIVE FUNCTIONS -------------------
 
   function getViewportDimensions() { 
     width = document.getElementsByTagName("main")[0].offsetWidth;
-    height = window.innerHeight;
-    height = height * 0.80;
-    widthInner = width * 0.80;
+    height = window.innerHeight * 0.8;
+    // height = height * 0.80;
+    widthInner = width * 0.97;
+
+
+
+
   };
 
   getViewportDimensions();
@@ -28,6 +32,9 @@
         width: width,
         height: height
       });
+
+    svg = svg.append("g")
+      .attr("transform", "translate(30,-20)");
   }
 
   drawSvg();
@@ -45,31 +52,29 @@
   // VISUALISATION -------------------
 
 
-  function visualise(data) {
-
-    console.log("hi");
 
     // rates.csv data has this date format: "2016-05-27"
     // %Y - year with century as a decimal number e.g.: 1999
     // %m - month as a decimal number e.g.: 12
     // %d - zero-padded day of the month as a decimal number e.g.: 31
+    
     var parseDate = d3.time.format("%Y-%m-%d").parse; 
 
     // time
     var xScale = d3.time.scale()
-      .domain(d3.extent(data, function(d) { return d.date; })) // could I simply use min and max?
       .range([0,widthInner]);
 
     var xAxis = d3.svg.axis()
+      .ticks(10)
       .scale(xScale)
       .orient("bottom");
 
     // interest rates
     var yScale = d3.scale.linear()
-      .domain([0, d3.max(data, function(d) { return d.rate; })])
       .range([height, 0]); 
 
     var yAxis = d3.svg.axis()
+      .ticks(7)
       .scale(yScale)
       .orient("left");
 
@@ -79,42 +84,58 @@
       .y0(height)
       .y1(function(d) { return yScale(d.rate); });
 
-    data.forEach(function(d) {
-      d.date = parseDate(d.date); 
-      d.rate = parseFloat(d.rate).toFixed(2); 
-    });
 
-    
-    console.log(data);
-    console.log(d);
 
-    svg.append("path")
-      .datum(data)
-      .attr("class", "area")
-      .attr("d", area);
-
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Rate (%)");
-
-  }
 
   // DATA -------------------
-  d3.csv("data/rates.csv", function(data){
-   if(data) visualise(data);
+  var q = d3_queue.queue();
 
-  })
+  q
+    .defer(d3.csv, "data/rates.csv") 
+    .await(function(error, data) { 
+      if(error){
+        console.log("error");
+      }
+      else{
+
+        data.forEach(function(d) {
+          d.date = parseDate(d.date); 
+          d.rate = parseFloat(d.rate).toFixed(2); 
+        });
+
+        console.log(d3.max(data, function(d){
+          return d.date;
+        }));
+
+        xScale 
+          .domain(d3.extent(data, function(d) { return d.date; })); // could I simply use min and max?
+
+        yScale
+         .domain([0, d3.max(data, function(d) { return d.rate; })]);
+
+        svg
+        .append("path")
+          .datum(data)
+          .attr("class", "area")
+          .attr("d", area);
+
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+        svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 5)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Rate (%)");
+        }  
+
+    });
 
 
 })();
